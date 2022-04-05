5e4e9fb865de9e3cf67a03c2e58dd79989a4171a
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-05 17:35:52 +0900

    feat: added first objc project.

---
+++ proj1/proj1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  proj1
+//
+//  Created by Kwangjun Kim on 2022/04/05.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Programming is fun!");
+    }
+    return 0;
+}
