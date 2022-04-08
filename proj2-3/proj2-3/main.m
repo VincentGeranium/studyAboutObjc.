7b025a58e1d586dcf701b5783e8d6c974436361f
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-08 17:56:13 +0900

    feat: added program 2.3

---
+++ proj2-3/proj2-3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  proj2-3
+//
+//  Created by Kwangjun Kim on 2022/04/08.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Testing...\n..1\n...2\n...3");
+    }
+    return 0;
+}
