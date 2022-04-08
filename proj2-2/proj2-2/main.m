a0e8e842c939cf54662cc5614e5cee40050907b2
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-08 17:50:26 +0900

    feat: added program 2.2

---
+++ proj2-2/proj2-2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  proj2-2
+//
+//  Created by Kwangjun Kim on 2022/04/08.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Programming is fun!");
+        NSLog(@"Programming in Objective-C is even more fun!");
+    }
+    return 0;
+}
