5a7c10ace019efc1516d38def3413894f31e2004
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-08 18:12:37 +0900

    feat: added homework 2.

---
+++ homework-2/homework-2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  homework-2
+//
+//  Created by Kwangjun Kim on 2022/04/08.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"In Objective-C, lowercase ltters are significant.\nmain is where program execution begins.\nOpen and closed braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.");
+    }
+    return 0;
+}
