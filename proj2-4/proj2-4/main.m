9e0133cee332acd909b05e8850608ed4203fb4b6
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-08 18:04:57 +0900

    feat: added program 2.4

---
+++ proj2-4/proj2-4/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  proj2-4
+//
+//  Created by Kwangjun Kim on 2022/04/08.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int sum;
+        
+        sum = 50 + 25;
+        
+        NSLog(@"The sum of 50 and 25 is %i", sum);
+    }
+    return 0;
+}
