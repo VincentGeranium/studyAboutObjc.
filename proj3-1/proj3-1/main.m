5640dfb4597e82d75d1835324cf2fcf771f00e04
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-17 16:56:40 +0900

    feat: added 3.2 category and proj 3-1, 3-2.

---
+++ proj3-1/proj3-1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  proj3-1
+//
+//  Created by Kwangjun Kim on 2022/04/17.
+//
+
+// 분수를 다루는 간단한 프로그램.
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int numerator = 1;
+        int denomintor = 3;
+        
+        NSLog(@"The fraction is %i/%i", numerator, denomintor);
+    }
+    return 0;
+}
