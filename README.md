d6670dcf2d2f276176a4795b841c823a61da7e4d
Kwangjun Kim <vincent@Kwangjunui-MacBookPro.local>
2022-04-17 16:57:50 +0900

    feat: added 3.3 category and proj3-1,3-2.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - - -
 - [220415 - 3.1 객체란?](https://www.notion.so/morgan-kang/3-1-9180d325f9ca44b489a3d35674786601)
 - [220415 - 3.2 인스턴스와 메서드.](https://www.notion.so/morgan-kang/3-2-94bea0e5410e40b58f48158db2f7b9da)
+- [220417 - 3.2 분수를 처리하는 Objective-C 클래스.](https://www.notion.so/morgan-kang/3-3-Objective-C-758c0f853efa4912a108cb786424e664)
--- proj3-2/proj3-2/main.m
+++ proj3-2/proj3-2/main.m
@@ -7,10 +7,59 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+// - - - @interface 부분 - - -
+
+@interface Fraction : NSObject
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+// - - - @implementation 부분 - - -
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n
+{
+    numerator = n;
+}
+
+-(void) setDenominator: (int) d
+{
+    denominator = d;
+}
+
+@end
+
+// - - - 프로그램 부분 - - -
+
+int main(int argc, char * argv[])
+{
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *myFraction;
+        
+        // Fraction 인스턴스를 생성한다.
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        // 1/3로 분수의 값을 설정한다.
+        [myFraction setNumerator: 1];
+        [myFraction setDenominator: 3];
+        
+        // print 메서드로. 분수의 값을 표시한다.
+        NSLog(@"The value of myFraction is:");
+        [myFraction print];
     }
     return 0;
 }
